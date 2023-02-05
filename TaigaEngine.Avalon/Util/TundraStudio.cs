61855452f9cf0e526be8b295a9fe11dff84fb950
Han Programer <30620423+Hanprogramer@users.noreply.github.com>
2023-02-05 12:06:40 +0700

    Create TundraStudio.cs

---
+++ TaigaEngine.Avalon/Util/TundraStudio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TundraEngine.Studio.Util
+{
+    public class TundraStudio
+    {
+        public static TundraProject? CurrentProject;
+    }
+}
